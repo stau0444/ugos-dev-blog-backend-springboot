4aea64f302282b64153fa7aab4ec81dcdf81709f
stau04@gmail.com <stau04@gmail.com>
2021-12-04 02:49:00 +0900

    load balancer test

--- src/main/java/com/project/ugosdevblog/controller/HomeController.java
+++ src/main/java/com/project/ugosdevblog/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.project.ugosdevblog.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,12 @@ public class HomeController {
 
     @GetMapping("/")
     public ResponseEntity healthCheck(){
-        return ResponseEntity.status(200).body(null);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,"*");
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,"*");
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS,"true");
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,"*");
+        return ResponseEntity.status(200)
+                .headers(headers).body("");
     }
 }
