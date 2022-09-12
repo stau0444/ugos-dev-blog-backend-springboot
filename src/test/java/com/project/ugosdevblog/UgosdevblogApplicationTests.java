a8d85a692f4ee2554d7220b5f934af1626c8e766
stau04@gmail.com <stau04@gmail.com>
2022-09-13 03:42:11 +0900

    fix error

--- src/test/java/com/project/ugosdevblog/UgosdevblogApplicationTests.java
+++ src/test/java/com/project/ugosdevblog/UgosdevblogApplicationTests.java
@@ -1,22 +1,13 @@
 package com.project.ugosdevblog;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UgosdevblogApplicationTests {
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
     @Test
     void contextLoads() {
 
-    }
-
-    @Test
-    void gitBranchTest(){
-        logger.info("added development branch");
     }
 
 }
